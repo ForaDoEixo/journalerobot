d284f73cdc3bb0a9b9836bbeb620067d446a9a55
Niv Sardi <xaiki@evilgiggle.com>
2018-04-14 13:50:24 -0300

    rework update.sh
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 branch=`git branch | grep '*' | awk '{print $2}'`
+
 git remote update
-if (git log ${branch}...origin/${branch} | grep commit); then
-        pid=`cat PIDFILE`
+updated=`git log ${branch}...origin/${branch} | grep commit`
+
+if (updated); then
         git pull --rebase
         yarn
-        yarn build
+fi
+
+yarn build
+
+if (updated); then
         start-stop-daemon --stop --pidfile PID
         start-stop-daemon --start -b -m --pidfile PID -d `realpath .` --exec /usr/bin/node index.js
 fi
