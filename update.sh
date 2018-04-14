eecf7819ba7e4a94574dd4352160d18449768c49
Niv Sardi <xaiki@evilgiggle.com>
2018-04-14 09:59:03 -0300

    only pull and killall if we got updated
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
-git pull --rebase
-yarn
-yarn build
+#!/bin/sh
+branch=`git branch | grep '*' | awk '{print $2}'`
+git remote update
+if (git log ${branch}...origin/${branch}); then
+        pid=`cat PIDFILE`
+        git pull --rebase
+        yarn
+        yarn build
+        start-stop-daemon --stop --pidfile PID
+        start-stop-daemon --start -b -m --pidfile PID -d `realpath .` --exec /usr/bin/node index.js
+fi
+
