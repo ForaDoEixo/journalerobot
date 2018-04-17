63b04c2d080873c53c2842ff1300cb052bdce5fe
Niv Sardi <xaiki@evilgiggle.com>
2018-04-17 07:16:21 -0400

    fix update script
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- update.sh
+++ update.sh
@@ -4,15 +4,15 @@ branch=`git branch | grep '*' | awk '{print $2}'`
 git remote update
 updated=`git log ${branch}...origin/${branch} | grep commit`
 
-if (updated); then
+if (test -n "$updated"); then
         git pull --rebase
         yarn
 fi
 
 yarn build
 
-if (updated); then
-        start-stop-daemon --stop --pidfile PID
-        start-stop-daemon --start -b -C -m --pidfile PID -d `realpath .` --exec /usr/bin/env DEBUG=tapa-bot* /usr/bin/node index.js
+if (test -n "$updated"); then
+        /sbin/start-stop-daemon --stop --pidfile PID
+        /sbin/start-stop-daemon --start -b -C -m --pidfile PID -d `realpath .` --exec /usr/bin/env DEBUG=tapa-bot* /usr/bin/node index.js
 fi
 
