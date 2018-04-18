2f2db8252bde5f36b4bbb4b9186af175572ef6c3
Niv Sardi <xaiki@evilgiggle.com>
2018-04-18 18:30:41 -0300

    search: 100% test coverage
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/search.js
+++ src/search.js
@@ -6,37 +6,39 @@ class fuzzySearch {
   constructor(
     name,
     items,
-    opts,
-    resolve = (e) => (e)
+    opts = {}
   ) {
     this.opts = Object.assign({}, {
       maxSearchResults: 3, // used for debug only
       maxDistance: 0.001 // this is set by trial and error
     }, opts)
+
+    if (!name) {
+      throw new Error('Need to provide a name')
+    }
+
+    if (!items) {
+      throw new Error('Need to instanciate with items to load')
+    }
+
     this.name = name
     this.debug = DebugFactory(`tapa-bot:search:${this.name}  \t`)
-
-    this.resolve = resolve
-    this.stringMap = this.load(items, opts)
+    this.load(items)
   }
 
   load(items) {
     this.debug('new stringMap', this.opts)
-    let stringMap = new StringMap(this.opts)
+    this.stringMap = new StringMap(this.opts)
     this.items = items
-    Object.keys(items).map(k => stringMap.add(k.replace(/\s+/g, '_'), k.replace(/\s+/g, '_')))
-    return stringMap
+    Object.keys(items).map(k => this.stringMap.add(k.replace(/\s+/g, '_'), k.replace(/\s+/g, '_')))
+    return this.stringMap
   }
 
   search(term) {
     term = term.replace(/\s+/g, '_')
     let res = this.stringMap.search(term)
-    if (!res) {
-      return Promise.reject(new Error('not found'))
-    }
-
-    if (res[0].distance > this.opts.maxDistance) {
-      return Promise.reject(new Error(`${this.name}: found with bad distance`))
+    if (!res || !res[0] || res[0].distance > this.opts.maxDistance) {
+      return Promise.reject(new Error(`${this.name}: not found`))
     }
 
     let k = res[0].value.replace(/_/g, ' ')
---
+++ tests/search.spec.js
@@ -0,0 +1,79 @@
+'use strict'
+
+/* eslint-disable no-unused-expressions */
+const { expect } = require('chai')
+const FuzzySearch = require('../src/search')
+
+const path = require('path')
+
+const cachedNewspapers = require(`${__dirname}/data/newspapers.json`)
+const cachedCountries = require(`${__dirname}/data/countries.json`)
+const cachedZones = require(`${__dirname}/data/zones.json`)
+
+let keys = Array.from('abcdefghijklmnopqrstuvwxyz')
+
+function itemify(keys) {
+  return keys.reduce((a, c) => (
+    Object.assign(a, {[c + c + c + c]: c})
+  ), {})
+}
+
+describe('search', function () {
+  describe('main object', function () {
+    it('should throw if no name provided', function () {
+      expect(() => (new FuzzySearch())).to.throw()
+      expect(() => (new FuzzySearch(null, []))).to.throw()
+      expect(() => (new FuzzySearch(undefined, []))).to.throw()
+    })
+    it('should throw if no items provided', function () {
+      expect(() => (new FuzzySearch('test'))).to.throw()
+      expect(() => (new FuzzySearch('test', null))).to.throw()
+      expect(() => (new FuzzySearch('test', undefined))).to.throw()
+    })
+  })
+  describe('load', function () {
+    it('should load new objects')
+    let f = new FuzzySearch('test', itemify(keys.slice(0, 10)))
+
+    f.load(itemify(keys.slice(10)))
+
+    let s = 'xxxx'
+    return  f.search(s)
+             .then(r => {
+               expect(r).to.be.an('object')
+               expect(r.result).to.equal('x')
+               expect(r.distance).to.equal(0)
+             })
+  })
+  describe('search', function () {
+    let f = new FuzzySearch('test', itemify(keys.slice(0, 10)))
+
+    it('should return exact match with 0 distance', function () {
+      let s = 'cccc'
+
+      return  f.search(s)
+               .then(r => {
+                 expect(r).to.be.an('object')
+                 expect(r.result).to.equal('c')
+                 expect(r.distance).to.equal(0)
+               })
+    })
+    it('should return match', function () {
+      let s = 'ccc'
+
+      return  f.search(s)
+               .then(r => {
+                 expect(r).to.be.an('object')
+                 expect(r.result).to.equal('c')
+                 expect(r.distance).is.below(0.01)
+               })
+    })
+
+    it('should throw if no match', function (done) {
+      let s = 'xxx'
+
+      f.search(s)
+       .catch(e => done())
+    })
+  })
+})
